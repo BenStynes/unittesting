3085f4038994014874983cdfb1408da3cb1b3ae5
Ben Stynes(Styner) <45253301+BenStynes@users.noreply.github.com>
2021-12-01 14:25:45 +0000

    assert stuff done

--- simpleasserttestsolution/TestProject/MainlineTest.cpp
+++ simpleasserttestsolution/TestProject/MainlineTest.cpp
@@ -10,16 +10,16 @@ int main()
 	Production p;
 	
 	p.lottery();
+	//test size is 6
 	assert(p.lotto.size() == 6);
 	for (auto num : p.lotto)
 	{
-		
-		
-		if (num >= 1 && num <= 46)
-		{
-			assert(true);
-		}
+			//TEST 1-46
+			assert(num >= 1 && num <= 46);
+
 		
 	}
+	//test duplicates( the way  set works it wont be above 6 if there are dupes)
+	assert(p.lotto.size() == 6);
 	cin.get();
 }
